4e932e0c796278d99a35d52ae5ed9088e79b3092
eitan baron <eitanbaron@outlook.com>
2022-05-02 15:16:46 +0300

    1

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,113 @@
-import './home.scss'
-import Navbar from '../../components/navbar/Navbar'
+import "./home.scss";
+import Navbar from "../../components/navbar/Navbar";
 
 const Home = () => {
   return (
-    <div className='home'>
-     <Navbar/>
-      </div>
-  )
-}
+    <div className="home">
+      <Navbar />
+      <img
+        width="100%"
+        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis
+      temporibus, vitae optio delectus repellat deserunt ipsa voluptates
+      molestiae corporis quidem, expedita eius nostrum a quia ipsam fugiat
+      mollitia nobis pariatur. Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Facilis temporibus, vitae optio delectus repellat
+      deserunt ipsa voluptates molestiae corporis quidem, expedita eius nostrum
+      a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Facilis temporibus, vitae optio delectus
+      repellat deserunt ipsa voluptates molestiae corporis quidem, expedita eius
+      nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor,
+      sit amet consectetur adipisicing elit. Facilis temporibus, vitae optio
+      delectus repellat deserunt ipsa voluptates molestiae corporis quidem,
+      expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem
+      ipsum dolor, sit amet consectetur adipisicing elit. Facilis temporibus,
+      vitae optio delectus repellat deserunt ipsa voluptates molestiae corporis
+      quidem, expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis
+      temporibus, vitae optio delectus repellat deserunt ipsa voluptates
+      molestiae corporis quidem, expedita eius nostrum a quia ipsam fugiat
+      mollitia nobis pariatur. Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Facilis temporibus, vitae optio delectus repellat
+      deserunt ipsa voluptates molestiae corporis quidem, expedita eius nostrum
+      a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Facilis temporibus, vitae optio delectus
+      repellat deserunt ipsa voluptates molestiae corporis quidem, expedita eius
+      nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor,
+      sit amet consectetur adipisicing elit. Facilis temporibus, vitae optio
+      delectus repellat deserunt ipsa voluptates molestiae corporis quidem,
+      expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem
+      ipsum dolor, sit amet consectetur adipisicing elit. Facilis temporibus,
+      vitae optio delectus repellat deserunt ipsa voluptates molestiae corporis
+      quidem, expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis
+      temporibus, vitae optio delectus repellat deserunt ipsa voluptates
+      molestiae corporis quidem, expedita eius nostrum a quia ipsam fugiat
+      mollitia nobis pariatur. Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Facilis temporibus, vitae optio delectus repellat
+      deserunt ipsa voluptates molestiae corporis quidem, expedita eius nostrum
+      a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Facilis temporibus, vitae optio delectus
+      repellat deserunt ipsa voluptates molestiae corporis quidem, expedita eius
+      nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor,
+      sit amet consectetur adipisicing elit. Facilis temporibus, vitae optio
+      delectus repellat deserunt ipsa voluptates molestiae corporis quidem,
+      expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem
+      ipsum dolor, sit amet consectetur adipisicing elit. Facilis temporibus,
+      vitae optio delectus repellat deserunt ipsa voluptates molestiae corporis
+      quidem, expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis
+      temporibus, vitae optio delectus repellat deserunt ipsa voluptates
+      molestiae corporis quidem, expedita eius nostrum a quia ipsam fugiat
+      mollitia nobis pariatur. Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Facilis temporibus, vitae optio delectus repellat
+      deserunt ipsa voluptates molestiae corporis quidem, expedita eius nostrum
+      a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Facilis temporibus, vitae optio delectus
+      repellat deserunt ipsa voluptates molestiae corporis quidem, expedita eius
+      nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor,
+      sit amet consectetur adipisicing elit. Facilis temporibus, vitae optio
+      delectus repellat deserunt ipsa voluptates molestiae corporis quidem,
+      expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem
+      ipsum dolor, sit amet consectetur adipisicing elit. Facilis temporibus,
+      vitae optio delectus repellat deserunt ipsa voluptates molestiae corporis
+      quidem, expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis
+      temporibus, vitae optio delectus repellat deserunt ipsa voluptates
+      molestiae corporis quidem, expedita eius nostrum a quia ipsam fugiat
+      mollitia nobis pariatur. Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Facilis temporibus, vitae optio delectus repellat
+      deserunt ipsa voluptates molestiae corporis quidem, expedita eius nostrum
+      a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Facilis temporibus, vitae optio delectus
+      repellat deserunt ipsa voluptates molestiae corporis quidem, expedita eius
+      nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor,
+      sit amet consectetur adipisicing elit. Facilis temporibus, vitae optio
+      delectus repellat deserunt ipsa voluptates molestiae corporis quidem,
+      expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem
+      ipsum dolor, sit amet consectetur adipisicing elit. Facilis temporibus,
+      vitae optio delectus repellat deserunt ipsa voluptates molestiae corporis
+      quidem, expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis
+      temporibus, vitae optio delectus repellat deserunt ipsa voluptates
+      molestiae corporis quidem, expedita eius nostrum a quia ipsam fugiat
+      mollitia nobis pariatur. Lorem ipsum dolor, sit amet consectetur
+      adipisicing elit. Facilis temporibus, vitae optio delectus repellat
+      deserunt ipsa voluptates molestiae corporis quidem, expedita eius nostrum
+      a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor, sit amet
+      consectetur adipisicing elit. Facilis temporibus, vitae optio delectus
+      repellat deserunt ipsa voluptates molestiae corporis quidem, expedita eius
+      nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor,
+      sit amet consectetur adipisicing elit. Facilis temporibus, vitae optio
+      delectus repellat deserunt ipsa voluptates molestiae corporis quidem,
+      expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem
+      ipsum dolor, sit amet consectetur adipisicing elit. Facilis temporibus,
+      vitae optio delectus repellat deserunt ipsa voluptates molestiae corporis
+      quidem, expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur.
+    </div>
+  );
+};
 
-export default Home
+export default Home;
