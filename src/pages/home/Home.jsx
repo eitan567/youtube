bd82e367dbf34c085e9721e685e0976dcbd121e5
eitan baron <eitanbaron@outlook.com>
2022-05-12 14:41:45 +0300

    1

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,111 +1,12 @@
 import "./home.scss";
 import Navbar from "../../components/navbar/Navbar";
+import Featured from "../../components/featured/Featured";
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      <img
-        width="100%"
-        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-        alt=""
-      />
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis
-      temporibus, vitae optio delectus repellat deserunt ipsa voluptates
-      molestiae corporis quidem, expedita eius nostrum a quia ipsam fugiat
-      mollitia nobis pariatur. Lorem ipsum dolor, sit amet consectetur
-      adipisicing elit. Facilis temporibus, vitae optio delectus repellat
-      deserunt ipsa voluptates molestiae corporis quidem, expedita eius nostrum
-      a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor, sit amet
-      consectetur adipisicing elit. Facilis temporibus, vitae optio delectus
-      repellat deserunt ipsa voluptates molestiae corporis quidem, expedita eius
-      nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor,
-      sit amet consectetur adipisicing elit. Facilis temporibus, vitae optio
-      delectus repellat deserunt ipsa voluptates molestiae corporis quidem,
-      expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem
-      ipsum dolor, sit amet consectetur adipisicing elit. Facilis temporibus,
-      vitae optio delectus repellat deserunt ipsa voluptates molestiae corporis
-      quidem, expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur.
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis
-      temporibus, vitae optio delectus repellat deserunt ipsa voluptates
-      molestiae corporis quidem, expedita eius nostrum a quia ipsam fugiat
-      mollitia nobis pariatur. Lorem ipsum dolor, sit amet consectetur
-      adipisicing elit. Facilis temporibus, vitae optio delectus repellat
-      deserunt ipsa voluptates molestiae corporis quidem, expedita eius nostrum
-      a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor, sit amet
-      consectetur adipisicing elit. Facilis temporibus, vitae optio delectus
-      repellat deserunt ipsa voluptates molestiae corporis quidem, expedita eius
-      nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor,
-      sit amet consectetur adipisicing elit. Facilis temporibus, vitae optio
-      delectus repellat deserunt ipsa voluptates molestiae corporis quidem,
-      expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem
-      ipsum dolor, sit amet consectetur adipisicing elit. Facilis temporibus,
-      vitae optio delectus repellat deserunt ipsa voluptates molestiae corporis
-      quidem, expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur.
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis
-      temporibus, vitae optio delectus repellat deserunt ipsa voluptates
-      molestiae corporis quidem, expedita eius nostrum a quia ipsam fugiat
-      mollitia nobis pariatur. Lorem ipsum dolor, sit amet consectetur
-      adipisicing elit. Facilis temporibus, vitae optio delectus repellat
-      deserunt ipsa voluptates molestiae corporis quidem, expedita eius nostrum
-      a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor, sit amet
-      consectetur adipisicing elit. Facilis temporibus, vitae optio delectus
-      repellat deserunt ipsa voluptates molestiae corporis quidem, expedita eius
-      nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor,
-      sit amet consectetur adipisicing elit. Facilis temporibus, vitae optio
-      delectus repellat deserunt ipsa voluptates molestiae corporis quidem,
-      expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem
-      ipsum dolor, sit amet consectetur adipisicing elit. Facilis temporibus,
-      vitae optio delectus repellat deserunt ipsa voluptates molestiae corporis
-      quidem, expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur.
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis
-      temporibus, vitae optio delectus repellat deserunt ipsa voluptates
-      molestiae corporis quidem, expedita eius nostrum a quia ipsam fugiat
-      mollitia nobis pariatur. Lorem ipsum dolor, sit amet consectetur
-      adipisicing elit. Facilis temporibus, vitae optio delectus repellat
-      deserunt ipsa voluptates molestiae corporis quidem, expedita eius nostrum
-      a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor, sit amet
-      consectetur adipisicing elit. Facilis temporibus, vitae optio delectus
-      repellat deserunt ipsa voluptates molestiae corporis quidem, expedita eius
-      nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor,
-      sit amet consectetur adipisicing elit. Facilis temporibus, vitae optio
-      delectus repellat deserunt ipsa voluptates molestiae corporis quidem,
-      expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem
-      ipsum dolor, sit amet consectetur adipisicing elit. Facilis temporibus,
-      vitae optio delectus repellat deserunt ipsa voluptates molestiae corporis
-      quidem, expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur.
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis
-      temporibus, vitae optio delectus repellat deserunt ipsa voluptates
-      molestiae corporis quidem, expedita eius nostrum a quia ipsam fugiat
-      mollitia nobis pariatur. Lorem ipsum dolor, sit amet consectetur
-      adipisicing elit. Facilis temporibus, vitae optio delectus repellat
-      deserunt ipsa voluptates molestiae corporis quidem, expedita eius nostrum
-      a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor, sit amet
-      consectetur adipisicing elit. Facilis temporibus, vitae optio delectus
-      repellat deserunt ipsa voluptates molestiae corporis quidem, expedita eius
-      nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor,
-      sit amet consectetur adipisicing elit. Facilis temporibus, vitae optio
-      delectus repellat deserunt ipsa voluptates molestiae corporis quidem,
-      expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem
-      ipsum dolor, sit amet consectetur adipisicing elit. Facilis temporibus,
-      vitae optio delectus repellat deserunt ipsa voluptates molestiae corporis
-      quidem, expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur.
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis
-      temporibus, vitae optio delectus repellat deserunt ipsa voluptates
-      molestiae corporis quidem, expedita eius nostrum a quia ipsam fugiat
-      mollitia nobis pariatur. Lorem ipsum dolor, sit amet consectetur
-      adipisicing elit. Facilis temporibus, vitae optio delectus repellat
-      deserunt ipsa voluptates molestiae corporis quidem, expedita eius nostrum
-      a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor, sit amet
-      consectetur adipisicing elit. Facilis temporibus, vitae optio delectus
-      repellat deserunt ipsa voluptates molestiae corporis quidem, expedita eius
-      nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem ipsum dolor,
-      sit amet consectetur adipisicing elit. Facilis temporibus, vitae optio
-      delectus repellat deserunt ipsa voluptates molestiae corporis quidem,
-      expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur. Lorem
-      ipsum dolor, sit amet consectetur adipisicing elit. Facilis temporibus,
-      vitae optio delectus repellat deserunt ipsa voluptates molestiae corporis
-      quidem, expedita eius nostrum a quia ipsam fugiat mollitia nobis pariatur.
+      <Featured />
     </div>
   );
 };
