c3ca584d55c29d9131ad89530c386537da2774ee
eitan baron <eitanbaron@outlook.com>
2022-05-19 15:11:46 +0300

    1

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -7,6 +7,12 @@ function Featured() {
         src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
         alt=""
       />
+      <span className="desc">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nam,
+        amet non nesciunt tenetur aspernatur eos ea, soluta quos ipsam obcaecati
+        sint reprehenderit minima delectus voluptas facilis adipisci quia
+        dolore?
+      </span>
     </div>
   );
 }
